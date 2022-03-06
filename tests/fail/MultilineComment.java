768680f3a3d3a0ab2c573f78b6807b672f338495
tbdk <tbdk@alumni.ku.dk>
2022-03-06 11:32:13 +0100

    Merge with main

---
+++ tests/fail/MultilineComment.java
@@ -0,0 +1,13 @@
+package pass;
+
+/* This is a multiline comment
+second line
+third line */
+
+public class A {
+     /*inserted random multiline comment*/ public A(int a){
+        return a;
+    }
+}
+/* multiline comment
+with no closing token
