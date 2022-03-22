18c45389938658e57bec16446ac80c4db8474e34
tbdk <tbdk@alumni.ku.dk>
2022-03-22 21:14:23 +0100

    Renamed test pass file to match class name and changed type of parameters to boolean

--- tests/pass/Ternary.java
+++ tests/pass/Ternary.java
@@ -1,7 +1,7 @@
 package pass;
 
 public class Ternary {
-    public int ternaryOp(int a, int b, int c, int d, int e) {
+    public int ternaryOp(boolean a, int b, boolean c, int d, int e) {
         // should be handled as a right-associative expression like this: a ? b : (c ? d : e)
         return a ? b : c ? d : e;
     }
