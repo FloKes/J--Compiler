efe69eeab94bd225465092e2f8e241f38e6c8155
utufan <h.tufanusta@gmail.com>
2022-04-07 12:42:17 +0200

    double added to type checking for -=

--- src/jminusminus/JAssignment.java
+++ src/jminusminus/JAssignment.java
@@ -234,6 +234,9 @@ class JMinusAssignOp extends JAssignment {
         if (lhs.type().equals(Type.INT)) {
             rhs.type().mustMatchExpected(line(), Type.INT);
             type = Type.INT;
+        } else if (lhs.type().equals(Type.DOUBLE)) {
+            rhs.type().mustMatchExpected(line(), Type.DOUBLE);
+            type = Type.DOUBLE;
         } 
         else {
             JAST.compilationUnit.reportSemanticError(line(),
