3cf6664d880f2ab420ae9f65d068a5e026f16c20
utufan <h.tufanusta@gmail.com>
2022-04-07 12:43:25 +0200

    double added to type checking for /=

--- src/jminusminus/JAssignment.java
+++ src/jminusminus/JAssignment.java
@@ -387,7 +387,10 @@ class JDivAssignOp extends JAssignment {
         if (lhs.type().equals(Type.INT)) {
             rhs.type().mustMatchExpected(line(), Type.INT);
             type = Type.INT;
-        } else {
+        } else if (lhs.type().equals(Type.DOUBLE)) {
+            rhs.type().mustMatchExpected(line(), Type.DOUBLE);
+            type = Type.DOUBLE;
+        }else {
             JAST.compilationUnit.reportSemanticError(line(),
                     "Invalid lhs type for /=: " + lhs.type());
         }
