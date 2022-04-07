486d5e271245c10ea7040aae4055243829331c97
utufan <h.tufanusta@gmail.com>
2022-04-07 12:44:01 +0200

    double added to type checking for %=

--- src/jminusminus/JAssignment.java
+++ src/jminusminus/JAssignment.java
@@ -460,6 +460,9 @@ class JRemAssignOp extends JAssignment {
         if (lhs.type().equals(Type.INT)) {
             rhs.type().mustMatchExpected(line(), Type.INT);
             type = Type.INT;
+        }else if (lhs.type().equals(Type.DOUBLE)) {
+            rhs.type().mustMatchExpected(line(), Type.DOUBLE);
+            type = Type.DOUBLE;
         } else {
             JAST.compilationUnit.reportSemanticError(line(),
                     "Invalid lhs type for %=: " + lhs.type());
