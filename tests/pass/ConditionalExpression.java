875f454c14e0778a778f4bfc13e0900a2a0aca4d
Krupinszki Balázs <krupybalu@gmail.com>
2022-04-21 11:03:57 +0200

    rename ConditionalExpression tests

--- tests/pass/ConditionalExpression.java
+++ tests/pass/ConditionalExpression.java
@@ -1,7 +1,7 @@
 package pass;
 
-public class Ternary {
-    public int ternaryOp(boolean a, int b, boolean c, int d, int e) {
+public class ConditionalExpression {
+    public int conditionalOp(boolean a, int b, boolean c, int d, int e) {
         // should be handled as a right-associative expression like this: a ? b : (c ? d : e)
         return a ? b : c ? d : e;
     }
