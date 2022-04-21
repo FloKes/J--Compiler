dce738514a1a440267aeb6355930b504d0aadcf4
utufan <h.tufanusta@gmail.com>
2022-04-21 10:24:14 +0200

    test file created for static init blocks

--- tests/pass/TryCatchFinally.java
+++ tests/pass/TryCatchFinally.java
@@ -4,6 +4,11 @@ public class TryCatchFinally {
     public void tryCatchFinally () {
         try {
             int x = 5;
+            try{
+                int z = 0;
+            } catch{
+                int z = 1;
+            }
         } catch (Exception e) {
             int x = 10;
         } finally {
