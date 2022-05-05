f4b85b0f5d48132de9cd6c3671db6da7f0a97bdf
Krupinszki Balázs <krupybalu@gmail.com>
2022-05-05 18:06:01 +0200

    interface test updated to accomodate complex stuff

--- tests/pass/Interfaces.java
+++ tests/pass/Interfaces.java
@@ -3,27 +3,27 @@ package pass;
 import java.lang.System;
 
 interface AnInterfaceA {
-    int a = 0;
+    String a = "a";
+    void x();
 }
 
 interface AnInterfaceB {
     ;
     ;
-    int b = 0;
+    String b = "b";
+    String c = AnInterfaceA.a + "c";
+    void x();
 }
 
 interface AnInterfaceC extends AnInterfaceA, AnInterfaceB {
-    int c = 0;
 }
 
 public class Interfaces implements AnInterfaceA, AnInterfaceB, AnInterfaceC {
 
     public static String message() {
-        return A.a + ", " + (new B()).b;
+        return AnInterfaceA.a + ", " + b;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Classes.message());
-    }
+    public void x() {}
 
 }
