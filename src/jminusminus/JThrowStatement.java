0cb2cb5d75c860250b5992aa86792f9f6f0fe42e
tbdk <tbdk@alumni.ku.dk>
2022-05-09 22:22:50 +0200

    Removed analyze code, so it's blank

--- src/jminusminus/JThrowStatement.java
+++ src/jminusminus/JThrowStatement.java
@@ -1,29 +1,25 @@
-// Copyright 2011 Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
-
 package jminusminus;
 
 import static jminusminus.CLConstants.*;
 
 /**
- * The AST node for a return-statement. If the enclosing method
- * is non-void, then there is a value to return, so we keep track
- * of the expression denoting that value and its type.
+ * The AST node for a throw-statement.
  */
 
 class JThrowStatement extends JStatement {
 
-    /** The returned expression. */
+    /** The expression that is thrown. */
     private JExpression expr;
 
     /**
-     * Constructs an AST node for a return-statement given its
-     * line number, and the expression that is returned.
+     * Constructs an AST node for a throw-statement given its
+     * line number, and the expression that is thrown.
      * 
      * @param line
-     *            line in which the return-statement appears
+     *            line in which the throw-statement appears
      *            in the source file.
      * @param expr
-     *            the returned expression.
+     *            the thrown expression.
      */
 
     public JThrowStatement(int line, JExpression expr) {
@@ -32,10 +28,6 @@ class JThrowStatement extends JStatement {
     }
 
     /**
-     * Analysis distinguishes between being in a constructor
-     * or in a regular method in checking return types. In the
-     * case of a return expression, analyze it and check types.
-     * Determine the (possibly void) return type.
      * 
      * @param context
      *            context in which names are resolved.
@@ -43,44 +35,7 @@ class JThrowStatement extends JStatement {
      */
 
     public JStatement analyze(Context context) {
-        MethodContext methodContext = context.methodContext();
-
-        // The methodContext can be null if return statement
-        // occurs in a block that is not within a method.
-        // For example, in the Java grammar, return statement, at least
-        // syntactically, can occur in a static block. But since
-        // j-- does not allow a block to occur outside of a
-        // method, we don't check for methodContext being null
-
-        if (methodContext.methodReturnType() == Type.CONSTRUCTOR) {
-            if (expr != null) {
-                // Can't return a value from a constructor
-                JAST.compilationUnit.reportSemanticError(line(),
-                    "cannot return a value from a constructor");
-            }
-        } else {
-            // Must be a method
-            Type returnType = methodContext.methodReturnType();
-            methodContext.confirmMethodHasReturn();
-            if (expr != null) {
-                if (returnType == Type.VOID) {
-                    // Can't return a value from void method
-                    JAST.compilationUnit.reportSemanticError(line(),
-                        "cannot return a value from a void method");
-                } else {
-                    // There's a (non-void) return expression.
-                    // Its type must match the return type of the method
-                    expr = expr.analyze(context);
-                    expr.type().mustMatchExpected(line(), returnType);
-                }
-            } else {
-                // The method better have void as return type
-                if (returnType != Type.VOID) {
-                    JAST.compilationUnit.reportSemanticError(line(),
-                        "missing return value");
-                }
-            }
-        }
+        
         return this;
     }
 
