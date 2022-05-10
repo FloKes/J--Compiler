0a8a156fea087a74b56e45f1920a5b6f375aab0d
utufan <h.tufanusta@gmail.com>
2022-05-10 15:33:30 +0200

    double for multiple operators

--- tests/pass/DoubleType.java
+++ tests/pass/DoubleType.java
@@ -4,6 +4,27 @@ public class DoubleType {
     public int testDouble () {
         double op1 = 8.0;
         double op2 = 1.0;
+        double op3 = op1 * op2;
+        double divide = 2.0;
+        op3 /= divide;
+        if(op3>3.99)
+            return 1;
+        return 0;
+    }
+    public int testDouble2 () {
+        double op1 = 8.0;
+        double op2 = 1.0;
+        double op3 = op1 + op2;
+        double divide = 5.0;
+        op3 %= divide;
+        if(op3>3.99)
+            return 1;
+        return 0;
+    }
+
+    public int testDouble3 () {
+        double op1 = 8.0;
+        double op2 = 1.0;
         double op3 = op1 + op2;
         double divide = 5.0;
         op3 %= divide;
