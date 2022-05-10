cfc4583533a2f00d18ba78c40078bb6215935634
Ayman <27450013+ayman00@users.noreply.github.com>
2022-05-10 21:39:40 +0200

    Everything is working except the javaCC test for BitwiseUnaryComplement tests

--- tests/pass/Throws.java
+++
@@ -1,9 +0,0 @@
-package pass;
-
-import java.lang.Exception;
-
-public class Throw {
-    public void _throws () throws Exception {
-            throw new Exception("Testing exceptions, yay!");
-    }
-}
